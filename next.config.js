72ed2d19acceb18bbce3cbf4a914e63d8e743a19
Hanibal Girmay <hanibalgirmay@gmail.com>
2023-05-15 15:09:19 -0700

    fix next.js config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   trailingSlash: true,
-  // output: "export",
+  output: "export",
   api: {
     bodyParser: false,
   },
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/frontend-task/:path*',
-        destination: '/api/:path*',
-      },
-    ]
-  },
-  basePath: '/frontend-task',
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/frontend-task/:path*',
+  //       destination: '/api/:path*',
+  //     },
+  //   ]
+  // },
+  basePath: isProd ? "https://hanibalgirmay.github.io/frontend-task/" : "",
 };
 
 module.exports = nextConfig;
